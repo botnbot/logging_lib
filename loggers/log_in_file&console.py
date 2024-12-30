9be76543517b9599ee0d525a6727e61706cb6c0b
Sergey Korobeynikov <skoro@smn35.com>
2024-12-30 12:34:28 +0300

    2 modules of log setup added

---
+++ loggers/log_in_file&console.py
@@ -0,0 +1,40 @@
+import logging
+from datetime import datetime
+from logging import getLogger, DEBUG, Formatter
+
+
+def setup_logger():
+    logfile_name = datetime.now().strftime('%Y-%m-%d')
+
+    logger = getLogger('prosto_logger')
+    logger.setLevel(DEBUG)
+
+    if not logger.handlers:
+
+        file_handler = logging.FileHandler(logfile_name)
+        file_handler.setLevel(DEBUG)
+
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(DEBUG)
+
+        formatter = Formatter("%(asctime)s - %(level)s - %(message)s")
+
+        file_handler.setFormatter(formatter)
+        stream_handler.setFormatter(formatter)
+
+        logger.addHandler(file_handler)
+        logger.addHandler(stream_handler)
+
+    return logger
+
+
+def log_messages(level, message):
+    logger = setup_logger()
+    if level == 'info':
+        logger.info(message)
+    elif level == 'warning':
+        logger.warning(message)
+    elif level == 'error':
+        logger.error(message)
+    else:
+        logger.debug(message)
